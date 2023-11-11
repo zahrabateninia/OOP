76d8cf6219a907a2570e563ffedb48435840e89f
zahrabateninia <zahrabatenin@gmail.com>
2023-11-11 08:39:29 +0330

    Remove the extra files

--- getWage.js
+++
@@ -1,8 +0,0 @@
-let baseSalary = 30_000;
-let rate = 20;
-let overTime = 10;
-
-function getWage(baseSalary, rate, overTime) {
-     return baseSalary + (rate * overTime);
-}
-console.log(getWage(baseSalary, rate, overTime))
