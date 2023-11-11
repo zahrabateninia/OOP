590434eefac3f081059d47490b7549057e9059df
zahrabateninia <zahrabatenin@gmail.com>
2023-11-11 17:25:55 +0330

    Read the local(private) function outside the object

---
+++ setter_and_getter/drawCircle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node 
+
+function Circle(radius){
+    this.radius = radius;
+    let defaultLocation = {
+        x:0,
+        y:0
+    };
+    this.defaultLocation = function{
+        return defaultLocation;
+    };
+
+    this.draw = function(){
+        computeOptimumLocation(0.1);
+        console.log("draw");
+    };
+}
+const circle = new Circle(1);
+circle.draw();
+
+circle.defaultLocation() // now we can read it, but again we can not modify it.
