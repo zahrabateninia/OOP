ebdc3c83aa5558b951708a7facc0d57cfdbbd839
zahrabateninia <zahrabatenin@gmail.com>
2023-11-11 13:23:58 +0330

    Create a factory function

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function () {
-        console.log("Draw");
-    }
+function createCircle(radius){
+    return {
+        radius,
+        draw: function () {
+            console.log("Draw");
+        }
+    };    
 };
 
+const circle = createCircle(1);
 circle.draw();
